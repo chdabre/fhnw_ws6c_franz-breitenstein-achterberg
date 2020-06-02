050442703f0cc0adaa08e5021ce1f24a65dc6ea9
René Achterberg <rene.achterberg@bluewin.ch>
2020-06-02 17:43:31 +0200

    add detail screen when single tap on movie

--- lib/screens/detail/components/cast_crew_tile.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:movieapp/models/movie.dart';
-import 'package:movieapp/services/db.dart';
-import 'package:movieapp/services/tmdb.dart';
-import 'package:movieapp/theme/style.dart';
-
-

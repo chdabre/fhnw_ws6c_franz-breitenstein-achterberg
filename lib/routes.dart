6267074a803be3a7cb31d2d46687dbd15bfe5d2f
Dario Breitenstein <online@imakethings.ch>
2020-06-03 14:36:44 +0200

    clean unnecessary imports

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:movieapp/screens/favourites/favourites.dart';
-import 'package:movieapp/screens/detail/detail.dart';
 import 'package:movieapp/screens/home/home.dart';
 import 'package:movieapp/screens/onboarding/genres.dart';
 
